10f046d96d5e527e92efdb7a331d68196383edfc
tuechki <dtuechki@gmail.com>
2017-05-29 01:53:05 +0300

    script for moving whole apple

---
+++ FruitNinja/Scripts/MoveWholeApple.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+static var make : boolean;
+var apple : Transform;
+var half : GameObject;
+var random: int;
+ 
+ function Start () {
+ 
+	apple = this.transform.parent;
+	random = 0;
+ }
+ 
+ function Update () {
+ 
+ 	switch(random){
+ 		case 0: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,-5,32), 0.2);
+ 				random++;
+ 			break;
+ 		case 1: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,10,32), 0.1);
+ 				random++;
+ 			break;
+ 		case 2: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(10,0,32), 0.15);
+ 				random = 0;
+ 			break;
+ 	}
+  
+  
+ }
+ 
+ function OnMouseOver(){
+    //if(Input.GetMouseButtonDown(0)){
+        Instantiate (half, Vector3(apple.transform.position.x,apple.transform.position.y, apple.transform.position.z) , Quaternion.identity);
+       Destroy(gameObject);
+    //}
+}
