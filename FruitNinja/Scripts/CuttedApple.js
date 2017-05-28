885aec3c38c903232c62eec24941fe73ce4c39b0
GeorgiNzm <georginzm@gmail.com>
2017-05-29 01:51:33 +0300

    Adding Scripts for Apple Spawner and CuttedApple

---
+++ FruitNinja/Scripts/CuttedApple.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var speed : float = 5.0f; 
+var cut : int;
+
+function Start () {
+
+	cut = 0;
+	yield WaitForSeconds(1);
+	cut = 1;
+}
+
+function Update () {
+
+	if (cut == 0) {
+		gameObject.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,0,32),   speed*Time.deltaTime);
+	}
+
+}
---
+++ FruitNinja/Scripts/Spawner.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+
+var apple : GameObject;
+
+function Start () {
+
+	InvokeRepeating("Spawn", 7, 7);
+	InvokeRepeating("Spawn2", 3, 3);
+	InvokeRepeating("Spawn3", 9, 9);
+	InvokeRepeating("Spawn4", 5, 5);
+}
+
+function Update () {
+
+}
+
+function Spawn() {
+
+	Instantiate (apple, Vector3(transform.position.x,transform.position.y, transform.position.z) , Quaternion.identity);
+}
+
+function Spawn2() {
+
+	Instantiate (apple, 
+	Vector3(transform.position.x - 1,transform.position.y + 1, transform.position.z) ,
+	Quaternion.identity);
+}
+
+function Spawn3() {
+
+	Instantiate (apple, 
+	Vector3(transform.position.x - 1,transform.position.y - 3, transform.position.z) ,
+	Quaternion.identity);
+}
+
+function Spawn4() {
+
+	Instantiate (apple, 
+	Vector3(transform.position.x,transform.position.y - 2, transform.position.z) ,
+	Quaternion.identity);
+}
