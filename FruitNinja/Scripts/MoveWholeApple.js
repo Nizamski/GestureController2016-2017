8997f52a1feebb259fe235afa21506deb60b044f
GeorgiNzm <georginzm@gmail.com>
2017-05-29 22:20:24 +0300

    Adding yellow and red apple to game

--- FruitNinja/Scripts/MoveWholeApple.js
+++ FruitNinja/Scripts/MoveWholeApple.js
@@ -1,36 +1,48 @@
-﻿#pragma strict
+#pragma strict
 
 static var make : boolean;
 var apple : Transform;
 var half : GameObject;
 var random: int;
+var yellow : Material;
+var red : Material;
+var yellowHalf : Material;
+var redHalf : Material;
+var currentMaterial : Material;
+
  
- function Start () {
+function Start () {
  
 	apple = this.transform.parent;
-	random = 0;
- }
+	random = Random.Range(1,4);
+	switch(random){
+		case 1: gameObject.renderer.material = yellow;
+				currentMaterial = yellowHalf;
+			break;
+		case 2: gameObject.renderer.material = red;
+				currentMaterial = redHalf;
+			break;
+	}
+}
  
- function Update () {
+function Update () {
  
- 	switch(random){
- 		case 0: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,-5,32), 0.2);
- 				random++;
- 			break;
- 		case 1: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,10,32), 0.1);
- 				random++;
- 			break;
- 		case 2: apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(10,0,32), 0.15);
- 				random = 0;
- 			break;
- 	}
-  
-  
- }
+   apple.transform.position = Vector3.MoveTowards(transform.position, Vector3(0,0,30), 0.2);
  
- function OnMouseOver(){
+}
+ 
+function OnMouseOver(){
     //if(Input.GetMouseButtonDown(0)){
-        Instantiate (half, Vector3(apple.transform.position.x,apple.transform.position.y, apple.transform.position.z) , Quaternion.identity);
-       Destroy(gameObject);
+   		
+       var wholeApple = Instantiate (half, 
+       Vector3(apple.transform.position.x,apple.transform.position.y, apple.transform.position.z) ,
+        Quaternion.identity);
+        var half1 = wholeApple.transform.GetChild(0).GetChild(0);
+        half1.renderer.material = currentMaterial;
+   		
+   		var half2 = wholeApple.transform.GetChild(1).GetChild(0);
+        half2.renderer.material = currentMaterial;
+        
+       Destroy(gameObject); 
     //}
 }
